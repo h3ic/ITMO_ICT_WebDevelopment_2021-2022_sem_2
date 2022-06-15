0bbe1c41b658b1302dc48c32436b5a76fca64361
Anton Alexandrin <krelesser@yandex.ru>
2022-06-15 12:27:30 +0300

    model tests

--- students/k33422/Alexandrin_Anton/dogs/back/dogshow/main/signals.py
+++ students/k33422/Alexandrin_Anton/dogs/back/dogshow/main/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, pre_save, pre_delete
 from django.dispatch import receiver
 from .models import *
+from django.db.models import ObjectDoesNotExist
 
 
 @receiver(post_save, sender=Participant)
@@ -11,11 +12,14 @@ def create_participant(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Participant)
 def update_participant_vaccination(sender, instance, **kwargs):
-    prev_instance = Participant.objects.get(id=instance.id)
-    instance.previous_vaccination = prev_instance.vaccinated
-    print(f'Vaccination info for {instance.name} updated: \n'
-          f'was: {instance.previous_vaccination}\n'
-          f'now: {instance.vaccinated}\n')
+    try:
+        prev_instance = Participant.objects.get(id=instance.id)
+        instance.previous_vaccination = prev_instance.vaccinated
+        print(f'Vaccination info for {instance.name} updated: \n'
+              f'was: {instance.previous_vaccination}\n'
+              f'now: {instance.vaccinated}\n')
+    except ObjectDoesNotExist:
+        pass
 
 
 @receiver(pre_delete, sender=Participant)
