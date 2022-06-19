4bc41d930dcb18f6adb0c76c3f311ae0308d8696
Anton Alexandrin <krelesser@yandex.ru>
2022-06-19 13:21:22 +0300

    dockerfile -> react; 4 docs

--- students/k33422/Alexandrin_Anton/dogs/back/dogshow/dogshow/settings.py
+++ students/k33422/Alexandrin_Anton/dogs/back/dogshow/dogshow/settings.py
@@ -104,8 +104,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        # 'HOST': 'db',
+        # 'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
